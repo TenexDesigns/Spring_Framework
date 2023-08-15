39b802bacbc871fff49e60d3268297b6cf1ea850
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-15 19:49:01 +0300

    Update 2_WHat_is_A_Dependcy.c

--- Dependecy_Injection/2_WHat_is_A_Dependcy.c
+++ Dependecy_Injection/2_WHat_is_A_Dependcy.c
@@ -35,7 +35,83 @@ public class Hello {
 
 In this example, the `kelvin` bean method takes three parameters (`name`, `age`, and `address`), which are considered dependencies. These dependencies can be obtained from other beans (e.g., `name()` and `age()`) or from objects defined within the application context.
 
-In summary, dependencies in Spring can be various types of objects (reference types) that a class requires to perform its functionality. They can be beans or non-beans and can be injected into a class through constructors, setters, or methods. The flexibility of dependency injection allows you to build more modular and maintainable applications.
+In summary, dependencies in Spring can be various types of objects (reference types) that a class requires to perform its functionality. 
+They can be beans or non-beans and can be injected into a class through constructors, setters, or methods. The flexibility of dependency injection allows 
+you to build more modular and maintainable applications.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+MORE EXPLANANTION
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+In Java, a dependency can refer to various types, including:
+
+1. **Class dependencies**: These are dependencies on classes. For example, a method that takes a `String` parameter has a dependency on the `String` class.
+
+2. **Interface dependencies**: These are dependencies on interfaces. For instance, a method that takes a `CharSequence` parameter can accept any class that implements the `CharSequence` interface, such as `String`, `StringBuffer`, or `StringBuilder`.
+
+3. **Method or field dependencies**: These are dependencies on specific methods or fields of an object. The class or interface of the object itself is not important, as long as it has the required method or field. For example, a method may depend on a method called `getFileName` in the class of an object passed as a parameter.
+
+Dependencies can be direct or indirect:
+
+- **Direct dependencies**: These are dependencies where one class directly depends on another class or interface.
+
+- **Indirect or transitive dependencies**: These are dependencies that arise when a class depends on another class, which in turn depends on yet another class. If you cannot use class A without class B, and you cannot use class B without class C, then you cannot use class A without class C either.
+
+In the context of Spring Dependency Injection, a dependency refers to an object that is required by another object. The dependency can be any object, including basic types like `String` or `int`, as well as complex objects. In Spring, dependencies are typically managed as beans within the ApplicationContext.
+
+To implement Dependency Injection in Spring, you can use various techniques, including:
+
+- **Constructor Injection**: Dependencies are provided via constructor parameters. This is a preferred approach for mandatory dependencies.
+
+- **Setter Injection**: Dependencies are provided via setter methods. This is useful for optional dependencies or when there are multiple dependencies.
+
+- **Field Injection**: Dependencies are directly assigned to fields using annotations. This approach is simpler but may have limitations in terms of testability and immutability.
+
+Here's an example of constructor injection using your provided code:
+
+```java
+@Configuration
+public class AppConfig {
+    @Bean
+    public Address address() {
+        return new Address(344, "Nairobi");
+    }
+    
+    @Bean
+    public Person person(Address address) {
+        return new Person(address);
+    }
+}
+```
+
+In this example, the `Person` bean has a dependency on the `Address` bean. The `Address` bean is passed as a parameter to the constructor of the `Person` class during bean creation.
+
+It's important to note that not all beans need to be defined within the `@Configuration` class. Beans can be defined in any class annotated with `@Component`, `@Service`, or `@Repository`. However, the `@Configuration` class is commonly used to define and configure beans.
+
+I hope this helps clarify the types of dependencies, how they are used in Spring Dependency Injection, and where they can be defined. Let me know if you have any further questions!
+
+
+
+
+
+
+
+
+
+
+
 
 
 ..
