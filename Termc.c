34e3dc54a9c984909616cab9b81ce5e94fc44c81
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-15 09:32:35 +0300

    Create Termc.c

---
+++ Termc.c
@@ -0,0 +1,32 @@
+Spring Container /Spring context/ IOC container- Manages Spring beans and thir life cycle
+
+   e.g         var context = new AnnotationConfigApplicationContext(Hello.class);
+            Here context becomes the spring container
+
+
+Types of Spring Container
+
+1.Bean Factory - Basic Spring Container
+2.Application Context - Advanced spring Container with enterprice-specific features
+     - Easy to use in web applications
+     - Easy internationalization
+     - Easy integration with SpringAOP
+3.Which one to use 
+     - Most enterpic applications use Application context
+     - Recommeneded for  web applications, web services ,Rest API and microservices
+
+
+Spring Bean - Anything managed by spring frawork
+
+
+
+
+
+
+
+
+
+
+
+
+
